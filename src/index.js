54fde2d3a65ab43b54a69095ae610681e5ec9662
Drew Keat <thedrewkeat@gmail.com>
2022-01-14 22:08:12 -0700

    add combineReducers to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
@@ -8,8 +8,10 @@ import App from "./App";
 import InitialReducer from "./Reducers/InitialReducer";
 import reportWebVitals from "./reportWebVitals";
 
+const rootReducer = combineReducers({initial: InitialReducer})
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(InitialReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
